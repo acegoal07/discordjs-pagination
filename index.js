56cdd3b9d59f541c74d8dbcdd48ca2b338da2298
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2021-12-22 21:31:19 +0000

    1.0.9

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const MessagePagination = require('@acegoal07/discordjs-pagination/lib/message')
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // pagination ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = pagination = async ({interaction, message, pages, buttonList, timeout = 12000}) => {
+  // Checks
   if (!pages) throw new Error("Missing pages");
   if (!buttonList) throw new Error("Missing buttons");
+  if (buttonList.length < 2) throw new Error("Need provide at least 2 buttons");
+  for (const button of buttonList) {if (button.style === "LINK") throw new Error("Link buttons are not supported please check what type of buttons you are using")}
+  // Pass on
   if (message === undefined) {return InteractionPagination(interaction, pages, buttonList, timeout)}
   else if (interaction === undefined) {return MessagePagination(message, pages, buttonList, timeout)}
   else {throw new Error("Please provide either interaction or message for the pagination to use")}
