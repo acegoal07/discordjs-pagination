5a5613aa91b8c535ca4f9511b447442c4354cf28
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2022-01-10 16:23:51 +0000

    1.1.5

--- readme.md
+++ readme.md
@@ -18,10 +18,8 @@
 
 <h1 id="about">About</h1>
 
-**1.0.9 contained changes that requires you to update your code to support the new version find the universal example <a href="#example">here</a> the change is very simple but makes using the dependency easier**
-
 Required dependencies: 
-- discord.js version 13.0.1^
+- discord.js version 13.5.0^
 
 To install use:
 ```js
--- util/disabledButtons.js
+++ util/disabledButtons.js
@@ -8,97 +8,23 @@ const {
 // Params ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * @param {MessageButton[]} buttonList
+ * @param {MessageButton[]} disabledButtonList
  * @return {MessageActionRow} Disabled message action row
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Disabler //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = disabledButtons = async (buttonList) => {
-    let disabledRow
-    if (buttonList.length === 2) {
-        const btn0 = new MessageButton()
-          .setLabel(buttonList[0].label)
-          .setStyle(buttonList[0].style)
-          .setDisabled(true)
-          .setCustomId('0');
-        const btn1 = new MessageButton()
-          .setLabel(buttonList[1].label)
-          .setStyle(buttonList[1].style)
-          .setDisabled(true)
-          .setCustomId('1');
-        disabledRow = new MessageActionRow()
-          .addComponents(btn0, btn1);
-      } 
-      if (buttonList.length === 3) {
-        const btn0 = new MessageButton()
-          .setLabel(buttonList[0].label)
-          .setStyle(buttonList[0].style)
-          .setDisabled(true)
-          .setCustomId('0');
-        const btn1 = new MessageButton()
-          .setLabel(buttonList[1].label)
-          .setStyle(buttonList[1].style)
-          .setDisabled(true)
-          .setCustomId('1');
-        const btn2 = new MessageButton()
-          .setLabel(buttonList[2].label)
-          .setStyle(buttonList[2].style)
-          .setDisabled(true)
-          .setCustomId('2');
-        disabledRow = new MessageActionRow()
-          .addComponents(btn0, btn1, btn2);
-      }
-      if (buttonList.length === 4) {
-        const btn0 = new MessageButton()
-          .setLabel(buttonList[0].label)
-          .setStyle(buttonList[0].style)
-          .setDisabled(true)
-          .setCustomId('0');
-        const btn1 = new MessageButton()
-          .setLabel(buttonList[1].label)
-          .setStyle(buttonList[1].style)
-          .setDisabled(true)
-          .setCustomId('1');
-        const btn2 = new MessageButton()
-          .setLabel(buttonList[2].label)
-          .setStyle(buttonList[2].style)
-          .setDisabled(true)
-          .setCustomId('2');
-        const btn3 = new MessageButton()
-          .setLabel(buttonList[3].label)
-          .setStyle(buttonList[3].style)
-          .setDisabled(true)
-          .setCustomId('3');
-        disabledRow = new MessageActionRow()
-          .addComponents(btn0, btn1, btn2, btn3);
-      }
-      if (buttonList.length === 5) {
-        const btn0 = new MessageButton()
-          .setLabel(buttonList[0].label)
-          .setStyle(buttonList[0].style)
-          .setDisabled(true)
-          .setCustomId('0');
-        const btn1 = new MessageButton()
-          .setLabel(buttonList[1].label)
-          .setStyle(buttonList[1].style)
-          .setDisabled(true)
-          .setCustomId('1');
-        const btn2 = new MessageButton()
-          .setLabel(buttonList[2].label)
-          .setStyle(buttonList[2].style)
-          .setDisabled(true)
-          .setCustomId('2');
-        const btn3 = new MessageButton()
-          .setLabel(buttonList[3].label)
-          .setStyle(buttonList[3].style)
-          .setDisabled(true)
-          .setCustomId('3');
-        const btn4 = new MessageButton()
-          .setLabel(buttonList[4].label)
-          .setStyle(buttonList[4].style)
-          .setDisabled(true)
-          .setCustomId('4');
-        disabledRow = new MessageActionRow()
-          .addComponents(btn0, btn1, btn2, btn3, btn4);
-      }
-    return disabledRow;
+   let disabledButtonList = []
+   let count = 0;
+   for (const button of buttonList) {
+      count += 1;
+      disabledButtonList.push(
+         new MessageButton()
+            .setLabel(`${button.label}`)
+            .setStyle(`${button.style}`)
+            .setDisabled(true)
+            .setCustomId(`${count}`)
+      );
+   }
+   return new MessageActionRow().addComponents(disabledButtonList);
 }
