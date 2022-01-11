fe48744170f7fb0fcbfafa4817f7e3743eccf67e
Alexander wood <acegoal07@gmail.com>
2022-01-11 13:14:01 +0000

    1.1.6

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const MessagePagination = require('@acegoal07/discordjs-pagination/lib/message')
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // pagination ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = pagination = async ({interaction, message, pages, buttonList, timeout = 12000}) => {
-  // Checks
-  if (!pages) throw new Error("Missing pages");
-  if (!buttonList) throw new Error("Missing buttons");
-  if (buttonList.length < 2) throw new Error("Need provide at least 2 buttons");
-  for (const button of buttonList) {if (button.style === "LINK") throw new Error("Link buttons are not supported please check what type of buttons you are using")}
-  // Pass on
-  if (message === undefined) {return InteractionPagination(interaction, pages, buttonList, timeout)}
-  else if (interaction === undefined) {return MessagePagination(message, pages, buttonList, timeout)}
-  else {throw new Error("Please provide either interaction or message for the pagination to use")}
+   // Checks
+   if (message === undefined && interaction === undefined) throw new Error("Please provide either interaction or message for the pagination to use");
+   if (!pages) throw new Error("Missing pages");
+   if (!buttonList) throw new Error("Missing buttons");
+   if (timeout < 1000) throw new Error("You have provided a timeout less then 1000ms which is not allowed");
+   for (const button of buttonList) {if (button.style === "LINK") throw new Error("Link buttons are not supported please check what type of buttons you are using")}
+   if (buttonList.length < 2) throw new Error("Need provide at least 2 buttons");
+   if (buttonList.length > 5) {
+      new Error("More than 5 buttons have been provided the extras will be removed");
+      buttonList = buttonList.slice(1, 5);
+   }
+   // Pass on
+   if (message === undefined) {return InteractionPagination(interaction, pages, buttonList, timeout)}
+   else if (interaction === undefined) {return MessagePagination(message, pages, buttonList, timeout)}
+   else {throw new Error("The pagination was unable to run")}
 }
