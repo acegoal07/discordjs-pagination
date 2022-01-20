9bc0477ed7302ef6c7445335a055905221ff6f8a
GhexterCortes <ghextershumies@gmail.com>
2022-01-20 23:20:57 +0800

    Add reply option to MessagePagination

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const InteractionPagination = require('@acegoal07/discordjs-pagination/lib/inter
 const MessagePagination = require('@acegoal07/discordjs-pagination/lib/message');
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // pagination ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-module.exports = pagination = async ({interaction, message, pages, buttonList, timeout = 12000}) => {
+module.exports = pagination = async ({interaction, message, replyMessage = false, pages, buttonList, timeout = 12000}) => {
    // Checks
    if (message === undefined && interaction === undefined) throw new Error("Please provide either interaction or message for the pagination to use");
    if (!pages) throw new Error("Missing pages");
@@ -20,9 +20,9 @@ module.exports = pagination = async ({interaction, message, pages, buttonList, t
    if (typeof message?.author === "object") {
       // Checks
       if (!message && !message.channel) throw new Error("Channel is inaccessible");
-      if (pages.length < 2) return message.channel.send({embeds: [pages[0]]});
+      if (pages.length < 2) return replyMessage ? message.reply({embeds: [pages[0]]}) : message.channel.send({embeds: [pages[0]]});
       // Run
-      return MessagePagination(message, pages, buttonList, timeout);
+      return MessagePagination(message, replyMessage, pages, buttonList, timeout);
    }
    // Interaction
    // Checks
