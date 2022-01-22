a8028e4345f435073061b9a0477abf94a8eb4fdb
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2022-01-22 20:55:22 +0000

    1.2.0

--- util/progressBarBuilder.js
+++ util/progressBarBuilder.js
@@ -3,19 +3,19 @@
 /**
  * @param {Number} pages
  * @param {Number} page
+ * @param {String} progressSlider
+ * @param {String} emptySlide
  * @returns {String} Progress bar
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Progress bar maker ////////////////////////////////////////////////////////////////////////////////////////////////////
-module.exports = progressBar = async(pages, page) => {
-   const slider = "▣";
-   const line = "▢";
-   // Progress maths#
+module.exports = progressBarBuilder = async(pages, page, progressSlider = "▣", emptySlider = "▢") => {
+   // Progress maths
    const progress = page + 1;
    const emptyProgress = pages - (page + 1);
    // Progress text
-   const progressText = slider.repeat(progress);
-   const emptyProgressText = line.repeat(emptyProgress);
+   const progressText = progressSlider.repeat(progress);
+   const emptyProgressText = emptySlider.repeat(emptyProgress);
    // Create bar
    return `[${progressText+emptyProgressText}] : Page ${page + 1}`;
 }
